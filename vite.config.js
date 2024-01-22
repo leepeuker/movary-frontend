191f2f514110988ce54d3b59cddee9e91c361f4f
JVT038 <47184046+JVT038@users.noreply.github.com>
2024-01-22 22:16:50 +0100

    Added PWA support (not tested tho)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,vue,png,svg,woff,woff2,scss}'],
+      },
+      manifest: {
+        name: 'Movary',
+        short_name: 'Movary',
+        description: 'Self hosted web app to track and rate your watched movies',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/movary-logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images/movary-logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
